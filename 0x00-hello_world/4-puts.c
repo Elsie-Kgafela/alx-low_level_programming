cdd669b6f8abf1a3a935634ff83faf6a3304d028
Elsie Kgafela <elckgafela@gmail.com>
2023-06-15 06:40:38 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(vold)
+{
+	puts["Programming is like building a multilingual puzzle\n"];
+	return (0);
+}
